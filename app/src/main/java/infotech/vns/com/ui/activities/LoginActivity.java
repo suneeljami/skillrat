c9380c96671e9ae6c5ae5ea756d96cf56deee5fd
Suneelkumar jami <suneel.j@dhanushinfotech.net>
2018-04-03 23:13:58 +0530

    initial commit

---
+++ app/src/main/java/infotech/vns/com/ui/activities/LoginActivity.java
@@ -0,0 +1,25 @@
+package infotech.vns.com.ui.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import infotech.vns.com.ui.R;
+
+public class LoginActivity extends AppCompatActivity {
+Button btn_login,btn_register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn_login=findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
