cb74d04c175064b935f4a42a2e8c7f5745a325af
Suneelkumar jami <suneel.j@dhanushinfotech.net>
2018-05-29 10:58:13 +0530

    adding google signin

--- app/src/main/java/infotech/vns/com/ui/activities/LoginActivity.java
+++ app/src/main/java/infotech/vns/com/ui/activities/LoginActivity.java
@@ -3,19 +3,42 @@ package infotech.vns.com.ui.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
 
 import infotech.vns.com.ui.R;
 
 public class LoginActivity extends AppCompatActivity {
-Button btn_login,btn_register;
+    private static final int RC_SIGN_IN = 1;
+    Button btn_login,btn_register;
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         btn_login=findViewById(R.id.btn_login);
         btn_register=findViewById(R.id.btn_register);
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,5 +51,60 @@ Button btn_login,btn_register;
                 startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
             }
         });
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        logintoApp(account);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            logintoApp(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("signin", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this, "signin failed", Toast.LENGTH_SHORT).show();
+//            logintoApp(null);
+        }
+    }
+    private void logintoApp(GoogleSignInAccount account){
+if (account!=null) {
+    Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+    loginIntent.putExtra("email", account.getEmail());
+    loginIntent.putExtra("name", account.getDisplayName());
+    startActivity(loginIntent);
+}
+else {
+    Toast.makeText(this, "failed login", Toast.LENGTH_SHORT).show();
+}
     }
 }
--- app/src/main/java/infotech/vns/com/ui/activities/MainActivity.java
+++ app/src/main/java/infotech/vns/com/ui/activities/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(getIntent().getStringExtra("name"));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
